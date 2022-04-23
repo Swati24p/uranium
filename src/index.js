69270941f1b2f34618ddc87faa4f9f4a3913e945
unknown <swatipathak699@gmail.com>
2022-04-23 23:00:17 +0530

    auth-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://swati_pathak:DGhDxlBIIfyRwGwk@cluster0.ogdpf.mongodb.net/SwATI-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.m
 app.use('/', route)
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 5000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 5000))
 });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,3 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+const validateToken = function(req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+    
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken) {
+      return res.send({ status: false, msg: "token is invalid" });
+    }
+
+    let userToBeModified = req.params.userId;
+    let userLoggedIn = decodedToken.userId;
+    if(userToBeModified != userLoggedIn) return res.send ({status: false,
+         msg: 'User logged is not allowed to modify the requested users data'})
+    
+
+    next()
+}
+
+
+module.exports.validateToken = validateToken
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
