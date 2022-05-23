eb8a04e735a10a977e8867734a486bdbf76602a2
unknown <swatipathak699@gmail.com>
2022-05-23 16:55:10 +0530

    done

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use( multer().any())
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 4000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 4000))
 });
